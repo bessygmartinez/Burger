e32e4f0547faf6ce05c91b902b640c69a14a3b34
Bessy Gee <nakiwarai13@gmail.com>
2019-12-28 18:04:37 -0500

    added functions insertOne and updateOne

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,23 @@
-require("../config/orm.js");
+let orm = require("../config/orm.js");
 
 let burger = {
     selectAll: function(cb){
         orm.selectAll('burgers', function(res) {
             cb(res);
         });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function(obj, condition, cb) {
+        orm.updateOne("burgers", obj, condition, function(res) {
+            cb(res);
+        });
     }
-}
+};
 
 module.exports = burger;
