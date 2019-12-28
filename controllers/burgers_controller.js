c23603c1e57a664ad319e8261d4b92d20918e389
Bessy Gee <nakiwarai13@gmail.com>
2019-12-28 18:03:32 -0500

    added app.post route to insert into table, and app.put route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-require("../models/burger.js");
+let burger = require("../models/burger.js");
 
 app.get('/', function(req, res) {
     burger.selectAll(function(data) {
@@ -8,6 +8,30 @@ app.get('/', function(req, res) {
             burgers: data
         };
         res.render('index', hbsObj);
+    });
+});
+
+app.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(data) {
+        res.json({id: data.insertId});
+    });
+});
+
+app.put("/api/burgers/:id", function(req, res) {
+    let condition = `id = ${req.params.id}`
+
+    console.log(`condition: ${condition}`);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+        if (result.chagedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
