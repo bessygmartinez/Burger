c660a7d192b84410cf75d0c1273a22550b8513ae
Bessy Gee <nakiwarai13@gmail.com>
2019-12-28 17:55:57 -0500

    added body-parser and express-handlebars to required files, set up static route, body parsing, handlebars views, and route listener

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 //Required files
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+let exphbs = require("express-handlebars");
 
 let PORT = process.env.PORT || 8080;
+
+//To server static content for the app from "public" directory
+app.use(express.static("public"));
+
+//Parse app
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Parse app/JSON
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import routes and give the server access
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`App now listening at localhost: ${PORT}`)
+})
