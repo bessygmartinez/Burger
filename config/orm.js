29943d626775b70dd2c0c8a066f6656799d4362f
Bessy Gee <nakiwarai13@gmail.com>
2019-12-28 18:02:15 -0500

    added functions to print question marks, send objects to MySQL, insert burger values into table, and update table

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,33 @@
-require("../config/connection.js");
+let connection = require("../config/connection.js");
+require("dotenv").config();
+
+function printQuestion(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++){
+        arr.push("?");
+    }
+
+    return arr.toString();
+};
+
+function objToSQL(ob) {
+    let arr = [];
+    for (let key in ob) {
+        let value = ob[key];
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = `'${value}'`;
+            }
+            arr.push(`${key} = ${value}`)
+        }
+    }
+    return arr.toString();
+};
 
 let orm = {
     selectAll: function (table, cb) {
-        let query = `SELECT * FROM ${process.dbTable};`
+        let query = `SELECT * FROM ${process.env.dbTable};`
         connection.query(query, [table], function(err, res) {
             if (err) {
                 console.log(err);
@@ -10,8 +35,42 @@ let orm = {
             }
             console.log(res);
             cb(res);
-        })
+        });
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+        let query = `INSERT INTO ${process.env.dbTable}`;
+        query += ' (';
+        query += cols.toString();
+        query += ') ';
+        query += 'VALUES (';
+        query += printQuestion(vals.length);
+        query += ') ';
+
+        connection.query(query, vals, function(err, res) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            cb(res);
+        });
+    },
+
+    updateOne: function(table, obj, condition, cb) {
+        let query = `UPDATE ${process.env.dbTable}`;
+        query += ' SET ';
+        query += objToSQL(obj);
+        query += ' WHERE ';
+        query += condition;
+
+        connection.query(query, function(err, res) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            cb(res);
+        });
     }
-}
+};
 
 module.exports = orm;
