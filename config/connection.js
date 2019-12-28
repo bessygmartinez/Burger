bc1dcc4f442a32137bec082b1c9e88554e295b2c
Bessy Gee <nakiwarai13@gmail.com>
2019-12-28 17:58:53 -0500

    establised connection to burgers_db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-//Create connection to bamazon database
+//Create connection to burgers database
 const connection = mysql.createConnection({
     host: process.env.dbServer,
     port: process.env.dbPort,
     user: process.env.dbUser,
     password: process.env.dbPass,
     database: process.env.db
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error(`error connection: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
