906d2b604addf03999104bb35599945316d8d41e
Bessy Gee <nakiwarai13@gmail.com>
2020-01-02 13:59:57 -0500

    deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
 const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
 //Create connection to burgers database
-const connection = mysql.createConnection({
+connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "Yesyesno1!",
     database: "burgers_db"
-});
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
