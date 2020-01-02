1bff14009d3613a1c5e0a462f436a4fbbfd35113
Bessy Gee <nakiwarai13@gmail.com>
2020-01-02 13:12:42 -0500

    committing for consistency

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
 CREATE TABLE burgers 
 (
 id INT NOT NULL AUTO_INCREMENT,
@@ -7,5 +8,3 @@ burger_name VARCHAR(255) NOT NULL,
 devoured BOOLEAN DEFAULT false,
 primary key (id)
 );
-
-SELECT * FROM burgers;
